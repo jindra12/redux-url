bf6e60c673da21210a11ff52c5a4e37813be4bcc
Jan Jindráček <jan.jindracek@vzp.cz>
2020-06-05 12:43:55 +0200

    Allow for custom callback from middleware

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -8,8 +8,8 @@ import { transformQueryToSearch } from '../utils/transform';
  * @param callback if user time-travels, this gets called to update rest of store.
  */
 export const urlMiddleware = (
-    callback?: (url: UrlStoreState) => AnyAction,
-): Middleware => <TStore extends Store & { url: UrlStoreState }>(
+    callback?: (url: UrlStoreState) => (AnyAction | void),
+): Middleware => <TStore extends Store>(
     _: MiddlewareAPI<Dispatch, TStore>
 ) => (next: Dispatch<AnyAction>) => (action: UrlAction) => {
     if (action.type === 'SET_URL_QUERY' && action.payload.search) {
@@ -19,7 +19,10 @@ export const urlMiddleware = (
         history.pushState(null, '', `#${action.payload.hash}`);
     }
     if (action.type === 'SET_NEW_URL' && callback) {
-        next(callback(action.payload));
+        const callbackResult = callback(action.payload);
+        if (callbackResult) {
+            next(callbackResult);
+        }
     }
     next(action);
 };
